b0ded6c87e34179bdbe375800392b8def797fbbd
Keleman Levon <extern.keleman.levon@tttech.com>
2020-11-25 04:00:03 +0100

    Modified Makefile.cmd, added ci/Jenkinsfile

--- Makefile.cmd
+++ Makefile.cmd
@@ -4,47 +4,11 @@
 
 @echo off
 
-if exist "printer.c" (
-    if exist "printer.h" (
-        echo "Compiling printer.c file..."
-    ) else (
-		echo "Missing printer.h file"
-		goto :bad
-	)
-) else (
-	echo "Missing printer.c file"
-	goto :bad
-)
-sleep 1
-echo "Done"
-sleep 1
-
-
-if exist "hello_world.c" (
-    if exist "hello_world.h" (
-        echo "Compiling hello_world.c file..."
-    ) else (
-		echo "Missing hello_world.h file"
-		goto :bad
-	)
-) else (
-	echo "Missing hello_world.c file"
-	goto :bad
-)
-sleep 1
-echo "Done"
-sleep 1
-
+echo "Compiling printer.c file..."
+echo "Compiling hello_world.c file..."
 
 echo "Linking final executable hello_world.exe..."
 echo.>"hello_world.exe"
-sleep 1
-echo "Done"
-sleep 1
 
-:good
 exit 0
-
-:bad
-exit 2
 
